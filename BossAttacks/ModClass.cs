a9e2fff9e802408a450ad5a20b8ea7f9d44473af
royitaqi <royitaqi@users.noreply.github.com>
2023-01-07 13:28:56 -0800

    Can load mod in game.

---
+++ BossAttacks/ModClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Modding;
+using UnityEngine;
+using Vasi;
+using UObject = UnityEngine.Object;
+
+namespace BossAttacks
+{
+    public class BossAttacks : Mod
+    {
+        internal static BossAttacks Instance;
+
+        ///
+        /// Mod
+        ///
+
+        // <breaking change>.<non-breaking major feature/fix>.<non-breaking minor feature/fix>.<patch>
+        public override string GetVersion() => VersionUtil.GetVersion<BossAttacks>();
+
+        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects)
+        {
+            Log("Initializing mod");
+
+            Instance = this;
+
+            Log("Initialized mod");
+        }
+    }
+}
---
+++ BossAttacks/ReadMe.md
@@ -0,0 +1,3 @@
+# BossAttacks
+
+A mod for the game Hollow Knight.
