be3671ac151d041c213c864e0df022bdb776032f
royitaqi <royitaqi@users.noreply.github.com>
2023-01-20 20:16:16 -0800

    Can simulate "left" by pressing "1", but only when on bench (i.e. when game pull "IsPressed" or "WasPressed")

--- BossAttacks/Debugger.cs
+++ BossAttacks/Debugger.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using BossAttacks.Modules;
 using BossAttacks.Utils;
+using InControl;
 using Modding;
-using Newtonsoft.Json;
+using MonoMod.RuntimeDetour;
 using UnityEngine;
 
 namespace BossAttacks
@@ -20,10 +17,64 @@ namespace BossAttacks
         public void Unload()
         {
             ModHooks.HeroUpdateHook -= OnHeroUpdate;
+            _isPressed?.Dispose();
+            _isPressed = null;
+            _wasPressed?.Dispose();
+            _wasPressed = null;
         }
-        
+
         private void OnHeroUpdate()
         {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                if (_isPressed == null)
+                {
+                    var isPressed = typeof(OneAxisInputControl).GetMethod("get_IsPressed");
+                    _isPressed = new Hook(isPressed, MyIsPressed);
+                }
+                if (_wasPressed == null)
+                {
+                    var wasPressed = typeof(OneAxisInputControl).GetMethod("get_WasPressed");
+                    _wasPressed = new Hook(wasPressed, MyWasPressed);
+                }
+                if (_inputHandler == null)
+                {
+                    _inputHandler = typeof(HeroController).GetField("inputHandler", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).GetValue(HeroController.instance) as InputHandler;
+                }
+
+                _down = true;
+                this.LogModTEMP($"1: {_down}");
+            }
+            else if (Input.GetKeyUp(KeyCode.Alpha1))
+            {
+                _down = false;
+                this.LogModTEMP($"1: {_down}");
+            }
         }
+
+        private bool MyIsPressed(Func<OneAxisInputControl, bool> orig, OneAxisInputControl self)
+        {
+            if (self == _inputHandler?.inputActions?.left)
+            {
+                this.LogModTEMP($"left (is): {_down}");
+                return _down;
+            }
+            return orig(self);
+        }
+
+        private bool MyWasPressed(Func<OneAxisInputControl, bool> orig, OneAxisInputControl self)
+        {
+            if (self == _inputHandler?.inputActions?.left)
+            {
+                this.LogModTEMP($"left (was): {_down}");
+                return _down;
+            }
+            return orig(self);
+        }
+
+        private InputHandler _inputHandler;
+        private Hook _isPressed;
+        private Hook _wasPressed;
+        private bool _down;
     }
 }
